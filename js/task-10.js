7aac6af3797d5ed6afd37c5cbc03c376ccf82dad
Krzysztof-GoIT <145707752+Krzysztof-GoIT@users.noreply.github.com>
2023-12-28 13:42:28 +0100

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,37 @@
+const destroyButton = document.querySelector("[data-destroy]");
+const createButton = document.querySelector("[data-create]");
+const boxes = document.querySelector("#boxes");
+
+// Destroy Boxes
+destroyButton.addEventListener("click", () => {
+  destroyBoxes();
+});
+
+function destroyBoxes() {
+  boxes.innerHTML = "";
+}
+
+createButton.addEventListener("click", () => {
+  const amount = Number(controls.querySelector("input").value);
+  createBoxes(amount);
+});
+
+// Create Boxes
+function createBoxes(amount) {
+  const boxSize = 30;
+  const fragment = document.createDocumentFragment();
+
+  for (let i = 0; i < amount; i++) {
+    const size = boxSize + i * 10;
+    const div = document.createElement("div");
+    div.style.cssText = `width: ${size}px; height: ${size}px; background-color: ${getRandomHexColor()};`;
+    fragment.appendChild(div);
+  }
+
+  boxes.appendChild(fragment);
+}
+
+// Color Generator
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
